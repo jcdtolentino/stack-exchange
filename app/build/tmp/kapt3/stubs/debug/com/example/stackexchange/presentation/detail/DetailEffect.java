2546a75177f125cf25deca3077b12fbb66d2eb21
John Tolentino <john@Johns-MacBook-Pro.local>
2026-02-19 19:38:41 +0800

    project clean up

--- app/build/tmp/kapt3/stubs/debug/com/example/stackexchange/presentation/detail/DetailEffect.java
+++
@@ -1,9 +0,0 @@
-package com.example.stackexchange.presentation.detail;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b7\u0018\u00002\u00020\u0001B\u0007\b\u0004\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/example/stackexchange/presentation/detail/DetailEffect;", "Lcom/example/stackexchange/presentation/base/UiEffect;", "()V", "app_debug"})
-public abstract class DetailEffect implements com.example.stackexchange.presentation.base.UiEffect {
-    
-    private DetailEffect() {
-        super();
-    }
-}
